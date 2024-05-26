47156ad0b2eadd84a06999625111d960fe0c9ebb
pedramcode <dev.dehghanpour@gmail.com>
2024-05-26 04:06:36 +0330

    rocket web server

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - **DATABASE_URL** _(string)_: the Postgres database URL
 - **MAX_DB_CONNECTION** _(number)_: maximum connections in pool
 - **SECRET** _(string)_: secret string for hashes, hmacs, jwt and etc.
+- **HTTP_HOST** _(string)_: HTTP server host address
+- **HTTP_PORT** _(number)_: HTTP server port number
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
+use std::{net::IpAddr, str::FromStr};
+
 pub mod config;
 pub mod entities;
 pub mod repos;
 pub mod services;
 pub mod utils;
 
-#[tokio::main]
-async fn main() {
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
     dotenvy::dotenv().expect("unable to find \".env\" file");
     sqlx::migrate!("./migrations")
         .run(&config::db::pool().await.clone())
         .await
         .expect("unable to migrate database");
+
+    let mut http_config = rocket::Config::default();
+    http_config.port = match std::env::var("HTTP_PORT") {
+        Ok(port) => port.parse::<u16>().unwrap(),
+        Err(_) => 8000,
+    };
+    let addr = match std::env::var("HTTP_HOST") {
+        Ok(addr) => addr,
+        Err(_) => String::from("127.0.0.1"),
+    };
+    http_config.address = IpAddr::from_str(addr.as_str()).unwrap();
+
+    let _rocket = rocket::build()
+        .configure(http_config)
+        .mount("/", rocket::routes![])
+        .launch()
+        .await?;
+
+    Ok(())
 }
